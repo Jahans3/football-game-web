90cac355123735d07e4ad4940c187caa3b8b13ff
Joshua Jahans <a9858965@9858965s-MacBook-Pro.local>
2019-01-29 21:48:05 +0000

    Update author.yaml

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Ghost',
-    description: 'The professional publishing platform',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: 'The Football Drinking Game',
+    description: '⚽️🍻👍',
+    siteUrl: 'https://www.thefootballdrinkinggame.com', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
