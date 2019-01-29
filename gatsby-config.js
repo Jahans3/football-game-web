2bfd501175e99ce3b132187b7549078aec56492a
Joshua Jahans <a9858965@9858965s-MacBook-Pro.local>
2019-01-29 21:38:48 +0000

    Setup analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-XXXX-Y',
+        trackingId: 'UA-131762070-2',
         // Puts tracking script in the head instead of the body
         head: true,
         // IP anonymization for GDPR compliance
